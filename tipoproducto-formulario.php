ae7dd579c2881782d6a2391989a828e6fff08fd1
pabloaturnes <pabloaturnes@gmail.com>
2021-04-14 19:27:18 -0300

    mensaje

--- tipoproducto-formulario.php
+++ tipoproducto-formulario.php
@@ -3,6 +3,8 @@
 include_once "config.php";
 include_once "entidades/tipoproducto.php";
 $pg = "Edición de tipo de productos";
+$msj= "";
+$value="";
 
 $tipoproducto = new Tipoproducto();
 $tipoproducto->cargarFormulario($_REQUEST);
@@ -12,15 +14,18 @@ if($_POST){
         if(isset($_GET["id"]) && $_GET["id"] > 0){
               //Actualizo un cliente existente
               $tipoproducto->actualizar();
-              header("Location: tipoproducto-formulario.php");
+              $msj= "El tipo de producto ha sido actualizado correctamente";
+              $value="alert-success";
         } else {
             //Es nuevo
             $tipoproducto->insertar();
-            header("Location: tipoproducto-formulario.php");
+            $msj= "El tipo de producto ha sido guardado correctamente";
+            $value="alert-primary";
         }
     } else if(isset($_POST["btnBorrar"])){
         $tipoproducto->eliminar();
-        header("Location: tipoproductos.php");
+        $msj= "El tipo de producto ha sido borrado correctamente";
+        $value="alert-danger";
     }
 } 
 if(isset($_GET["id"]) && $_GET["id"] > 0){
@@ -36,12 +41,22 @@ include_once("header.php");
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800">Tipo de productos</h1>
            <div class="row">
-                <div class="col-12 mb-3">
+                <div class="col-6 mb-3">
                     <a href="tipoproductos.php" class="btn btn-primary mr-2">Listado</a>
                     <a href="tipoproducto-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                     <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
                     <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
                 </div>
+                <?php if($msj) {  ?>
+                    <div class="col-6 mb-3">
+                        <div class="alert <?php echo $value; ?> alert-dismissible fade show" role="alert">
+                            <strong> <?php echo $msj; ?></strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                <?php } ?>
             </div>
             <div class="row">
                 <div class="col-12 form-group">
--- venta-formulario.php
+++ venta-formulario.php
@@ -79,7 +79,7 @@ include_once("header.php");
           <!-- Page Heading -->
           <h1 class="h3 mb-4 text-gray-800">Venta</h1>
             <div class="row">
-                <div class="col-12 mb-3">
+                <div class="col-6 mb-3">
                     <a href="ventas.php" class="btn btn-primary mr-2">Listado</a>
                     <a href="venta-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
                     <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
